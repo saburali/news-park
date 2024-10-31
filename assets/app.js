fd3cd14921ae6b84fff231e7be8094b387f5798b
saburali <saburali.me@gmail.com>
2024-10-31 21:29:01 +0600

    Added `jQuery` & Setup Topbar Trending Carousel.

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.trending-item').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    });
+});
