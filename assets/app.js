f2db7b8c6562fae07b293d0ecfda52cb0b6b9bea
saburali <saburali.me@gmail.com>
2024-10-31 23:29:32 +0600

    Header menu added using `stellarnav`.

--- assets/app.js
+++ assets/app.js
@@ -10,4 +10,6 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     });
+
+    jQuery('.stellarnav').stellarNav();
 });
